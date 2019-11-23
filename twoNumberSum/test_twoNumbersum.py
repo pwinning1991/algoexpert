997cafc012c205cef7214ec7801a646d3fda7f73
Philip Winnington <ftcpjw@yahoo.com>
2019-11-23 16:06:51 -0500

    added unit testing to function

--- twoNumberSum/test_twoNumbersum.py
+++ twoNumberSum/test_twoNumbersum.py
@@ -5,7 +5,12 @@ class TestProgram(unittest.TestCase):
     def test_case_1(self):
         output = sorted(twoNumbersSum([4,6], 10))
         self.assertEqual(output, [4,6])
-
+    def test_case_2(self):
+        output = sorted(twoNumbersSum([4,6], 5))
+        self.assertEqual(output, [])
+    def test_case_3(self):
+        output = sorted(twoNumbersSum([4,6,5,-3,-5,33,103,1], 5))
+        self.assertEqual(output, [1,4])
 
 if __name__ == '__main__':
     unittest.main()
