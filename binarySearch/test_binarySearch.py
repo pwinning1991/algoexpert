625bec5bd5525cf27dbc9b59b22f6b2c8ed40b3a
Philip Winnington <ftcpjw@yahoo.com>
2019-11-23 17:58:21 -0500

    added testing

--- binarySearch/test_binarySearch.py
+++ binarySearch/test_binarySearch.py
@@ -6,6 +6,10 @@ class TestBinarySearch(unittest.TestCase):
         self.assertEqual(binarySearch([1,5,23,111], 111),3)
     def test_case_2(self):
         self.assertEqual(binarySearch([1,5,23,111], 101),-1)
+    def test_case_3(self):
+        self.assertEqual(binarySearch([-5,-1,11,23,111], 11),2)
+    def test_case_4(self):
+        self.assertEqual(binarySearch([-5,-1,11,23,33,56,112], 112),6)
 
 
 
