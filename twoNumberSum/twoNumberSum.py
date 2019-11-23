62589a5d814ee32638961a6d1e097f2504f9b2b0
Philip Winnington <ftcpjw@yahoo.com>
2019-11-23 16:22:21 -0500

    added second way of completing

--- twoNumberSum/twoNumberSum.py
+++ twoNumberSum/twoNumberSum.py
@@ -9,3 +9,20 @@ def twoNumbersSum(array, targetSum):
         else:
             nums[num] = True
     return []
+
+
+def twoNumbersSum1(array, targetSum):
+    array.sorted()
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum == targetSum:
+            return [array[left],array[right]] 
+        elif currentSum < targetSum:
+            left += 1
+        elif currentSum > targetSum:
+            right -= 1
+    return []
+
+
